d495415f10a04cfe6bdbedb8b77f7b86db7d78bc
VittoRose <135705006+VittoRose@users.noreply.github.com>
2023-10-09 10:19:05 +0200

    Fingers poses

--- 22_DOF_Hand/main.m
+++ 22_DOF_Hand/main.m
@@ -7,8 +7,8 @@ clear; close all; clc;
 DOF = 14;
 
 % Link length
-L = [31 31.5 0; 51 25 22; 51 29 23; 50 26 20; 42 24 19];
-offset = [-25 0; 0 30; .3 1.3; .4 1.4; .5 1.5];
+L = [31 31.5 0; 50 25 20; 51 20 25; 50 25 20; 45 25 20];
+offset = [-20 0; 0 20; 3 27; 4 27; 5 23];
 
 % Initial configuration 
 var = 0;
@@ -18,11 +18,11 @@ var = 0;
 %     pi/2+var pi/2+var pi/2+var ...
 %     pi/2+var pi/2+var pi/2+var ]';
 
-q0 = [pi-0.2  ...
-    pi/2+0.4 pi-0.4 pi/2+var ...
-    pi/2+var pi/2+var pi/2+var ...
-    pi/2+var pi/2+var pi/2+var ...
-    pi/2+var pi/2+var pi/2+var ]';
+q0 = [pi-0.4 -pi/6-.1 ...
+    1.4/2*pi   pi/4+0.2   pi/4 ...
+    pi/2+0.2 0.1 0.1 ...
+    pi/2+0.1 0.1 0.1 ...
+    pi/2 0.1 0.1 ]';
 
 %% --- simulation parameters --- %%
 Ts = 0.05;
@@ -46,11 +46,14 @@ i = 1;
 q = q0;
 q_out(:,1) = q0;
 
+plot_hand(q,L, offset)
+axis equal
+
 % while (norm(des_pinky)>0.015 || norm(des_ring)>0.015 || norm(des_middle)>0.015...
 %         || norm(des_index)>0.015 || norm(des_thumb)>0.015) && i<max_step
 %     
 %     q = q_out(:,i); 
-    [thumb, index, middle, ring, pinky] = hand_direct_kinematics(q, L, offset);
+%   [thumb, index, middle, ring, pinky] = hand_direct_kinematics(q, L, offset);
 % 
 %     %-- errors --%
 %     e_thumb = des_thumb - thumb;
--- 22_DOF_Hand/plot_hand.m
+++ 22_DOF_Hand/plot_hand.m
@@ -2,17 +2,17 @@ function plot_hand(q, L, offset)
     [thumb, index, middle, ring, pinky] = hand_direct_kinematics(q, L, offset);
 
     % -- thumb plot -- %
-    plot([offset(1,1) thumb(1,1) thumb(1,2)],[offset(1,2) thumb(2,1) thumb(2,2)], '-ro', 'linewidth', 2);
+    plot([offset(1,1) thumb(1,1) thumb(1,2)],[offset(1,2) thumb(2,1) thumb(2,2)], '-ko', 'linewidth', 2);
     hold on
     
     % -- index plot -- %
-    plot([offset(2,1) index(1,1) index(1,2) index(1,3)],[offset(2,2) index(2,1) index(2,2) index(2,3)], '-bo', 'linewidth', 2);
+    plot([offset(2,1) index(1,1) index(1,2) index(1,3)],[offset(2,2) index(2,1) index(2,2) index(2,3)], '-ko', 'linewidth', 2);
 
     % -- middle plot -- %
-    plot([offset(3,1) middle(1,1) middle(1,2) middle(1,3)],[offset(3,2) middle(2,1) middle(2,2) middle(2,3)], '-yo', 'linewidth', 2);
+    plot([offset(3,1) middle(1,1) middle(1,2) middle(1,3)],[offset(3,2) middle(2,1) middle(2,2) middle(2,3)], '-ko', 'linewidth', 2);
        
     % -- ring plot -- %
-    plot([offset(4,1) ring(1,1) ring(1,2) ring(1,3)],[offset(4,2) ring(2,1) ring(2,2) ring(2,3)], '-go', 'linewidth', 2);
+    plot([offset(4,1) ring(1,1) ring(1,2) ring(1,3)],[offset(4,2) ring(2,1) ring(2,2) ring(2,3)], '-ko', 'linewidth', 2);
     
     % -- pinky plot -- %
     plot([offset(5,1) pinky(1,1) pinky(1,2) pinky(1,3)],[offset(5,2) pinky(2,1) pinky(2,2) pinky(2,3)], '-ko', 'linewidth', 2);
